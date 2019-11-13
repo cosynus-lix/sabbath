52058b112ea9c17d4e01dde5939c51e70f34f6aa
SannyaBTM <sannya.amoikon@polytechnique.edu>
2019-11-13 17:20:58 +0100

    minor changes

--- barrier/test/test_printers.py
+++ barrier/test/test_printers.py
@@ -25,7 +25,6 @@ class testprint(TestCase):
 
     def test_printer_2(self):       
         """Test case 2 with integers"""
-        case2 = [ "\n[Test 2:integers]", "\n(x1,x2)", "\n0\n"]
         x1, x2 = [Symbol("x%s" % (i+1), REAL) for i in range(2)] 
         formula = Implies( GE(x1+x2,Real(2)*x2), GE(x1,Real(-1)*x2))
         PysmtToQepcadPrinter(formula,"TestingFormulasQepcad.txt")
@@ -37,7 +36,6 @@ class testprint(TestCase):
 
     def test_printer_3(self):       
         """Test case 3 with reals"""
-        case3 = [ "\n[Test 3: reals]", "\n(x1,x2)", "\n0\n"]
         x1, x2 = [Symbol("x%s" % (i+1), REAL) for i in range(2)] 
         formula = ForAll([x1,x2],Implies( GE(x1+x2,Real(3.5)*x2), GE(x1,Real(24.6)*x2)))
         PysmtToQepcadPrinter(formula,"TestingFormulasQepcad.txt")
@@ -49,7 +47,6 @@ class testprint(TestCase):
     
     def test_printer_4(self):
         """Test case 4 : quantifiers succession"""
-        case = [ "\n[Test 4: quantifiers]", "\n(x1,x2)", "\n0\n"]
         x1, x2 = [Symbol("x%s" % (i+1), REAL) for i in range(2)] 
         formula = ForAll([x1],Exists([x2],Implies( GE(x1+x2,Real(3.5)*x2), GE(x1,Real(24.6)*x2))))
         PysmtToQepcadPrinter(formula,"TestingFormulasQepcad.txt")
@@ -61,7 +58,6 @@ class testprint(TestCase):
 
     def test_printer_5(self):
         """Test case 5 : testing =""" 
-        case = [ "\n[Test 5: = ]", "\n(x2)", "\n0\n"]
         x2 = Symbol("x2", REAL)
         formula = Equals(x2,Real(3))
         PysmtToQepcadPrinter(formula,"TestingFormulasQepcad.txt")
@@ -75,7 +71,6 @@ class testprint(TestCase):
      
     def test_printer_6(self):
         """Test case 6 : testing not""" 
-        case = [ "\n[Test 6: ~ ]", "\n(A)", "\n0\n"]
         A = Symbol("A", BOOL)
         formula = Not(A)
         PysmtToQepcadPrinter(formula,"TestingFormulasQepcad.txt")
@@ -88,7 +83,6 @@ class testprint(TestCase):
 
     def test_printer_7(self):
         """Test case 7 : testing /=""" 
-        case = [ "\n[Test 7: /= ]", "\n(x2)", "\n0\n"]
         x2 = Symbol("x2", REAL)
         formula = Not(Equals(x2,Real(3)))
         PysmtToQepcadPrinter(formula,"TestingFormulasQepcad.txt")
@@ -101,7 +95,6 @@ class testprint(TestCase):
 
     def test_printer_8(self):
         """Test case 8 : testing div""" 
-        case = [ "\n[Test 8: / ]", "\n(x2)", "\n0\n"]
         x2 = Symbol("x2", REAL)
         formula = Div(x2,Real(3))
         PysmtToQepcadPrinter(formula,"TestingFormulasQepcad.txt")
@@ -115,7 +108,6 @@ class testprint(TestCase):
         """Test case 9 : testing boolean connectives: AND,OR, IMPLIES,
         IFF, NOT
         """ 
-        case = [ "\n[Test 9: bool connections ]", "\n(x1,x2)", "\n0\n"]
         x2 = Symbol("x2", REAL)
         x1 = Symbol("x1",REAL)
         formula_1 = LE(x2,Real(3))
