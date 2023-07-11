3653e30c12d955fe350d448ad6a9400f19dccede
battista_fbk <lbattista@fbk.eu>
2023-07-11 17:29:39 +0200

    Typos

--- expeval/build_piecewise_lyap_commands.py
+++ expeval/build_piecewise_lyap_commands.py
@@ -36,12 +36,12 @@ if __name__ == "__main__":
         fout.write(f"--output {outputname} --normalize_lyap_in_sdp_problem {normalization} --sdp-solver {sdp_solver} ")
         fout.write("\n")
     # eof
-    for n in Ns:
+    for n in [5]: # [3,5,10,15,18]
         for validation_method in ['smt']: # ['sylvester', 'sympy', 'smt']
             for normalization in [False]: # ['True', 'False']
-                for sdp_solver in ['cvxopt', 'mosek', 'smcp']: # ['cvxopt', 'mosek', 'smcp']
+                for sdp_solver in ['mosek', 'smcp']: # ['cvxopt', 'mosek', 'smcp']
                     if validation_method == "smt":
-                        for solver in ['z3', 'mathematica']: # ['mathematica', 'z3', 'mathsat', 'cvc5']
+                        for solver in ['mathematica']: # ['mathematica', 'z3', 'mathsat', 'cvc5']
                             write(n, validation_method, solver, normalization, sdp_solver)
                     else:
                         write(n, validation_method, 'z3', normalization, sdp_solver)
